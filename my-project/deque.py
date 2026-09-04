f390a2a7d2d433577004c8eef0e92125b18becf5
Fr1ze <Mizu1305@yandex.ru>
2026-09-04 12:51:40 +0300

    сделан шаблон

---
+++ my-project/deque.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class Deque():
+
+    def __init__(self, len):
+
+        self.arr = np.zeros(len)
+        self.right = len//2
+        self.left = len//2 - 1
+
+
+    def add_left(self):
+        pass
+
+    def add_right(self):
+        pass
+
+    

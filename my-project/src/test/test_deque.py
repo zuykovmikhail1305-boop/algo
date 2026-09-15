c4e0b5082220c89336a348bd79d97ae326df855a
Fr1ze <Mizu1305@yandex.ru>
2026-09-15 11:44:12 +0300

    l

--- my-project/src/test/test_deque.py
+++ my-project/src/test/test_deque.py
@@ -2,7 +2,42 @@ import pytest
 from my_project.deque import Deque_ended
 
 
-d = Deque_ended(10)
+def test_add_right():
+    d = Deque_ended(5)
+    d.add_right(1)
+    d.add_right(2)
+    assert d[0] == 1
+    assert d[1] == 2
+
+
+def test_add_left():
+    d = Deque_ended(5)
+    d.add_left(1)
+    d.add_left(2)
+    assert d[0] == 2
+    assert d[1] == 1
+
+
+def test_pop_right():
+    d = Deque_ended(5)
+    d.add_right(1)
+    d.add_right(2)
+    d.pop_right()
+    assert d[0] == 1
+
+
+def test_pop_left():
+    d = Deque_ended(5)
+    d.add_left(1)
+    d.add_left(2)
+    d.pop_left()
+    assert d[0] == 2
 
 
 
+def test_getitem():
+    d = Deque_ended(5)
+    d.add_right(1)
+    d.add_right(2)
+    assert d[0] == 1
+    assert d[1] == 2
